d0298964d795d4bba45d8dc4463217beabe0cf33
SaulBest <734207@student.dusd.net>
2023-05-25 10:21:12 -0700

    Finale Number 2

---
+++ Fallout Project 2023/Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    public const string TAG_CURRENCY = "Currency";
+    public const string TAG_EQUIPMENT = "Equipment";
+}
