113cd70823c4183ab8dfcf2cf987409323edd5ad
SaulBest <734207@student.dusd.net>
2023-05-25 20:54:05 -0700

    change

--- Fallout Project 2023/Assets/Scripts/Constants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Constants : MonoBehaviour
-{
-    public const string TAG_CURRENCY = "Currency";
-    public const string TAG_EQUIPMENT = "Equipment";
-}
